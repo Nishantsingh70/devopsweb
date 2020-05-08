f7590d4ffe5bec6fa959c068113fde3513d66350
Nishantsingh70 <nishantsingh9527@gmail.com>
2020-05-09 01:01:00 +0530

    9th  edit

--- php1.php
+++ php1.php
@@ -1,5 +1,5 @@
 <?php
 
-print 'Hello World %%% I am nishant and i am from shahjahanpur. I am 22 years old';
+print 'Hello World #### I am nishant and i am from shahjahanpur. I am 22 years old';
 
 ?>
